daa0a6d3488eab893ac381d1ff27a960dce33710
HyunLuna <128806349+HuynhLuan1610@users.noreply.github.com>
2024-07-12 23:46:10 +0700

    con

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.pw8iopd.mongodb.net',
+    DB_USER: 'bill1610',
+    DB_PASS: 'bill1610',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'caigicungduoc',
+    JWT_EXPIRES: '360000000000000000000000000', // in milliseconds
+    EMAIL_USER: 'denden1610000@gmail.com', // gmail service
+    EMAIL_PASS: 'ksqw aymx aons unyj'
+  };
+  module.exports = MyConstants;
